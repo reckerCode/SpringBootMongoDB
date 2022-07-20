0ab2d67043521a26ed8f218da7941943077652ce
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-20 11:26:18 -0400

    refactored for sonarlint errors

--- src/main/java/io/spring/mongo/MongoIntegration/MongoIntegrationApplication.java
+++ src/main/java/io/spring/mongo/MongoIntegration/MongoIntegrationApplication.java
@@ -1,7 +1,6 @@
 package io.spring.mongo.MongoIntegration;
 
 import io.spring.mongo.MongoIntegration.model.Customer;
-import io.spring.mongo.MongoIntegration.repository.CustomerRepository;
 import io.spring.mongo.MongoIntegration.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,31 +14,31 @@ import java.util.List;
 @Slf4j
 public class MongoIntegrationApplication implements CommandLineRunner {
 
-	@Autowired
-	private CustomerService customerService;
+    @Autowired
+    private CustomerService customerService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongoIntegrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongoIntegrationApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		customerService.deleteAllCustomers();
+    @Override
+    public void run(String... args) throws Exception {
+        customerService.deleteAllCustomers();
 
-		//save a couple of customers
-		customerService.saveCustomer(new Customer("Alice", "Smith"));
-		customerService.saveCustomer(new Customer("Bob", "Smith"));
+        //save a couple of customers
+        customerService.saveCustomer(new Customer("Alice", "Smith"));
+        customerService.saveCustomer(new Customer("Bob", "Smith"));
 
-		List<Customer> customers = customerService.getAllCustomersByLastName("Smith");
-		//fetching all the customers
-		log.info("Customers found with findAllByLastName(): ");
-		customers.forEach(System.out::println);
+        List<Customer> customers = customerService.getAllCustomersByLastName("Smith");
+        //fetching all the customers
+        log.info("Customers found with findAllByLastName(): ");
+        customers.forEach(customer -> log.info(customer.toString()));
 
-		log.info("Customer found with findByFirstname('Alice'): {}",
-				customerService.getCustomerByFirstName("Alice"));
+        log.info("Customer found with findByFirstname('Alice'): {}",
+                customerService.getCustomerByFirstName("Alice"));
 
-		log.info("Customers found with findAll(): ");
-		customerService.findAllCustomers();
-	}
+        log.info("Customers found with findAll(): ");
+        customerService.findAllCustomers();
+    }
 
 }
